20fce4d975c153f9e3960d75b0b6054a87d4a551
longdv ph29867 <longdvph29867@gmail.com>
2024-03-27 20:38:03 +0700

    update

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,11 +1,26 @@
 import React, { useContext } from "react";
 import { ProductListData } from "../App";
 import { DemoData } from "../types";
+import axios from "axios";
 
 type Props = {};
 
 const ProductList = (props: Props) => {
   const [productList, setProductList] = useContext(ProductListData);
+
+  const handleDelete = (id: string) => {
+    axios
+      .get("http://localhost:8000/products/" + id, { method: "DELETE" })
+      .then((response) => {
+        const newPorductList = productList.filter(
+          (item: DemoData) => item.id !== id
+        );
+        setProductList(newPorductList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <table className="table">
       <thead>
@@ -16,6 +31,7 @@ const ProductList = (props: Props) => {
           <th scope="col">desc</th>
           <th scope="col">price</th>
           <th scope="col">category</th>
+          <th scope="col">action</th>
         </tr>
       </thead>
       <tbody>
@@ -27,6 +43,18 @@ const ProductList = (props: Props) => {
             <td>{item.desc}</td>
             <td>{item.price}</td>
             <td>{item.category}</td>
+            <td>
+              <button type="button" className="btn btn-warning text-warning">
+                Edit
+              </button>
+              <button
+                type="button"
+                onClick={() => handleDelete(item.id as string)}
+                className="btn btn-danger text-danger"
+              >
+                Delete
+              </button>
+            </td>
           </tr>
         ))}
       </tbody>
